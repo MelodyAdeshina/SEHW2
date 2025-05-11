4a0d5cf6df8ed4a33d071d3ea189d9ccff19c171
Melody Adeshina <melodyadeshina@melodys-air.trinity.local>
2025-05-11 15:29:25 -0500

    Add all project files

---
+++ Database/connection.js
@@ -0,0 +1,17 @@
+// database.js file.
+var mysql = require('mysql2');
+ 
+require('dotenv').config(); // use npm install for this first
+ 
+var connection = mysql.createConnection({
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME
+});
+connection.connect((err => {
+    if(err) throw err;
+    console.log('MySQL connected successfully');
+}));
+ 
+exports.databaseConnection = connection;
